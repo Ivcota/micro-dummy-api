53cea003eaba353f0a329fab7dd732b8184d255b
Ivcota <ivcotad@gmail.com>
2021-07-14 07:21:59 -0700

    Post CRUD functionality - Errors on update and delete
    Error: UnhandledPromiseRejectionWarning: Unhandled promise rejection.

--- controllers/posts.js
+++ controllers/posts.js
@@ -42,16 +42,45 @@ exports.createPosts =
 // @access  PUBLIC
 exports.getPostsId =
   ("/:id",
-  (req, res) => {
-    res.send("Gets post by id");
+  async (req, res) => {
+    try {
+      const posts = await Post.findByPk(req.params.id);
+      res.status(200).json({
+        success: true,
+        data: posts,
+      });
+    } catch (error) {
+      res.send(error);
+    }
   });
 
-// @desc    Edits Post by ID
+// @desc    Edits Post by ID - Error on return data
 // @type    PUT
 // @access  PUBLIC
 exports.updatePosts =
   ("/:id",
-  (req, res) => {
+  async (req, res) => {
+    try {
+      const posts = await Post.update(
+        {
+          posts: req.body.posts,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+      // Return results -- Could be inefficient
+      const results = await Post.findByPk(req.params.id);
+      res.status(200).json({
+        success: true,
+        data: results,
+      });
+    } catch (error) {
+      res.send(error);
+    }
+
     res.send("Edits Post by ID");
   });
 
@@ -60,6 +89,19 @@ exports.updatePosts =
 // @access  PUBLIC
 exports.deletePosts =
   ("/:id",
-  (req, res) => {
+  async (req, res) => {
+    try {
+      const posts = await Post.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      res.json({
+        success: true,
+        data: {},
+      });
+    } catch (error) {}
+
     res.send("Deletes Post by ID");
   });
